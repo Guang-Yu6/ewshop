e5c5442254ee4760a3d0eb4a03c1c6936db8384f
Guang-Yu6 <everyting-day@foxmail.com>
2021-10-16 15:40:22 +0800

    公共样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img src="~assets/images/001.jpg" alt="">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from 'components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      // imgsrc:require('assets/images')
+    }
+  },
   components: {
     HelloWorld
   }
 }
 </script>
+
+<style>
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
